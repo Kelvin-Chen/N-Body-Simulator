cfd8346b8d57bbf5fe5093b850dc149269b48397
Kelvin Chen <kelvin@kelvinchen.org>
2014-09-21 16:05:29 -0400

    Implement the main physics loop

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,34 @@
-function mainLoop() : void {
-}
+/// <reference path='stage.ts' />
+/// <reference path='barnes-hut.ts' />
 
-window.requestAnimationFrame(mainLoop);
+module Main {
+    var run: boolean;
+
+    function mainLoop(): void {
+        setTimeout(function () {
+            stage.render();
+
+            // Pause/unpause loop
+            if (!run) return window.requestAnimationFrame(mainLoop);
+
+            // Actions for each frame
+            stage.update();
+
+            window.requestAnimationFrame(mainLoop);
+        }, 1000/60); // Cap at 60 FPS
+    }
+
+    // Get key events
+    window.onkeypress = (e: KeyboardEvent): void => {
+        switch (String.fromCharCode(e.which)) {
+            case ' ':
+                run = !run;
+                break;
+        }
+    }
+
+    var stage = new Stage.Stage(window.innerWidth, window.innerHeight);
+
+    // Start main loop
+    window.requestAnimationFrame(mainLoop);
+}
