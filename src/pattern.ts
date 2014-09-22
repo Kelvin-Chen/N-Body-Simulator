f843e597d4089c3c86987981c2597a01f3663e2a
Kelvin Chen <kelvin@kelvinchen.org>
2014-09-22 15:09:15 -0400

    Make square scale to screen size

--- src/pattern.ts
+++ src/pattern.ts
@@ -25,8 +25,8 @@ module Pattern {
         var bodies: BarnesHut.Body[] = [];
         for (var i = -25; i < 25; ++i) {
             for (var j = -25; j < 25; ++j) {
-                var x = width / 2 + i * 10;
-                var y = height / 2 + j * 10;
+                var x = width / 2 + i * height / 50;
+                var y = height / 2 + j * height / 50;
                 bodies.push(new BarnesHut.Body(new BarnesHut.Point(x, y), 1));
             }
         }
